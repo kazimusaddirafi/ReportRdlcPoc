8e6d5fb2e3380ad71fc4cca279090f24e6f85f44
rafiext <musaddi.rafi.ext@holcim.com>
2024-09-07 19:00:09 +0600

    Add project files.

---
+++ ReportRdlcPoc/Controllers/ReportController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReportRdlcPoc.Service;
+using System.Net.Mime;
+
+namespace ReportRdlcPoc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+
+        private IReportService _reportService;
+        private IWebHostEnvironment Environment;
+
+        public ReportController(IReportService reportService, IWebHostEnvironment _environment)
+        {
+            _reportService = reportService;
+            this.Environment = _environment;
+        }
+        [HttpGet("{reportName}/{reportType}")]
+        public ActionResult Get(string reportName, string reportType)
+        {
+            var reportNameWithLang = reportName;
+            var reportFileByteString = _reportService.GenerateReportAsync(reportNameWithLang, reportType);
+            return File(reportFileByteString, MediaTypeNames.Application.Octet, getReportName(reportNameWithLang, reportType));
+        }
+
+        [HttpGet("print")]
+        public IActionResult Print()
+        {
+            string mimetype = "";
+            int extension = 1;
+            var path = $"{this.Environment.ContentRootPath}\\ReportFiles\\Report1.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            //parameters.Add("ReportParameter1", "RDLC in Blazor Web Application.");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+            return File(result.MainStream, "application/pdf");
+        }
+
+
+        private string getReportName(string reportName, string reportType)
+        {
+            var outputFileName = reportName + ".pdf";
+
+            switch (reportType.ToUpper())
+            {
+                default:
+                case "PDF":
+                    outputFileName = reportName + ".pdf";
+                    break;
+                case "XLS":
+                    outputFileName = reportName + ".xls";
+                    break;
+                case "WORD":
+                    outputFileName = reportName + ".doc";
+                    break;
+            }
+
+            return outputFileName;
+        }
+    }
+}
---
+++ ReportRdlcPoc/Service/ReportService.cs
@@ -0,0 +1,84 @@
+﻿using AspNetCore.Reporting;
+using System.Reflection;
+using System.Text;
+
+namespace ReportRdlcPoc.Service
+{
+    public interface IReportService
+    {
+        byte[] GenerateReportAsync(string reportName, string reportType);
+    }
+  
+        
+
+        public class ReportService : IReportService
+        {
+
+            public byte[] GenerateReportAsync(string reportName, string reportType)
+            {
+
+                string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                string fileDirectory = Assembly.GetExecutingAssembly().Location.Replace("ReportRdlcPoc.dll", string.Empty);
+                string rdlcFilePath = string.Format("{0}ReportFiles\\{1}.rdlc", fileDirectory, reportName);
+
+         
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                Encoding.GetEncoding("utf-8");
+
+
+                if (!File.Exists(rdlcFilePath))
+                {
+                    throw new FileNotFoundException($"The report file was not found at path: {rdlcFilePath}");
+                }
+
+                // Register the necessary encoding provider
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                Encoding.GetEncoding("utf-8");
+
+                LocalReport report = new LocalReport(rdlcFilePath);
+
+                // Create parameter values
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+                parameters.Add("ReportParameter1", "BURO Bangladesh");
+
+
+                //List<UserDto> userList = new List<UserDto>();
+
+
+
+                //var user1 = new UserDto { FirstName = "jp", LastName = "jan", Email = "jp@gm.com", Phone = "+976666661111" };
+                //var user2 = new UserDto { FirstName = "jp2", LastName = "jan", Email = "jp2@gm.com", Phone = "+976666661111" };
+                //var user3 = new UserDto { FirstName = "Test", LastName = "Test", Email = "jp3@gm.com", Phone = "+976666661111" };
+                //var user4 = new UserDto { FirstName = "Test", LastName = "Test", Email = "jp4@gm.com", Phone = "+976666661111" };
+                //var user5 = new UserDto { FirstName = "jp5", LastName = "jan", Email = "jp5@gm.com", Phone = "+976666661111" };
+
+                //userList.Add(user1);
+                //userList.Add(user2);
+                //userList.Add(user3);
+                //userList.Add(user4);
+                //userList.Add(user5);
+
+                //report.AddDataSource("DataSet1", userList);
+
+
+                var result = report.Execute(GetRenderType(reportType), 1, parameters);
+
+
+                return result.MainStream;
+            }
+
+            private RenderType GetRenderType(string reportType)
+            {
+                return reportType.ToUpper() switch
+                {
+                    "XLS" => RenderType.Excel,
+                    "WORD" => RenderType.Word,
+                    _ => RenderType.Pdf
+                };
+            }
+        }
+    
+}
